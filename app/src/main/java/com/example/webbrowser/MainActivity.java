fd41ad4271ad500ca4acd6fe583959ca486f3ae6
Aditya <adityakumar2898@gmail.com>
2020-09-29 14:16:10 +0530

    Fixed Website Loading

--- app/src/main/java/com/example/webbrowser/MainActivity.java
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +19,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final EditText sedit=(EditText)findViewById(R.id.search_edit);
         Button sbutton=(Button)findViewById(R.id.search_button);
-        sbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String data=sedit.getText().toString();
-                Intent intent=new Intent(getApplicationContext(),webview.class);
-                intent.putExtra("message",data);
-                startActivity(intent);
+        sbutton.setOnClickListener(view -> openWebsite(sedit.getText().toString()));
+        sedit.setOnEditorActionListener((v, actionId, event) -> {
+            if(actionId == EditorInfo.IME_ACTION_GO) {
+                openWebsite(sedit.getText().toString());
+                return true;
             }
+            return false;
         });
     }
 
+    private void openWebsite(String data) {
+        Intent intent=new Intent(getApplicationContext(),webview.class);
+        intent.putExtra("message",data);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/webbrowser/webview.java
+++ app/src/main/java/com/example/webbrowser/webview.java
@@ -1,17 +1,27 @@
 package com.example.webbrowser;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.URLUtil;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.SearchView;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Pattern;
+
 public class webview extends AppCompatActivity {
     WebView webView;
     SearchView searchView;
+
+    private final String WEBSITE_REGEX = "^(http:\\/\\/|https:\\/\\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[\u200C\u200Ba-z]{3}\\.([a-z]+)?$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +34,31 @@ public class webview extends AppCompatActivity {
         webView.getSettings().setLoadWithOverviewMode(true);
         Intent intent = getIntent();
         final String str = intent.getStringExtra("message");
-        webView.loadUrl("https://www.google.com/search?q="+str);
+        if (null != str && Pattern.matches(WEBSITE_REGEX, str)) {
+            System.out.println(URLUtil.guessUrl(str).replace("http", "https"));
+            webView.loadUrl(URLUtil.guessUrl(str).replace("http", "https"));
+        } else {
+            webView.loadUrl("https://www.google.com/search?q="+str);
+        }
+
+        webView.setWebViewClient(new WebViewClient() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return false;
+            }
+        });
+
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-                webView.loadUrl("https://www.google.com/search?q="+searchView.getQuery());
+                if (null != s && Pattern.matches(WEBSITE_REGEX, s)) {
+                    webView.loadUrl(URLUtil.guessUrl(str).replace("http", "https"));
+                } else {
+                    webView.loadUrl("https://www.google.com/search?q="+s);
+                }
                 return false;
             }
 
